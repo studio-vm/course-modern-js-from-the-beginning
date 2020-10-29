9fefc6603d3516780a09f452a48953460bb50b50
Valentin Manaila <valentin.manaila@gmail.com>
2020-10-29 15:43:25 +0000

    Section 4 - implement clear tasks and filter tasks functionality

--- 4-DOM Projects/Tasklist/app.js
+++ 4-DOM Projects/Tasklist/app.js
@@ -15,12 +15,18 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-// Load all event listeners
+//^ Load all event listeners
 loadEventListeners();
 
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  //Remove task event
+  taskList.addEventListener('click', removeTask);
+  //Clear tasks event
+  clearBtn.addEventListener('click', clearTasks);
+  //Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 //Add task function
@@ -40,7 +46,7 @@ function addTask(e) {
   const link = document.createElement('a');
   link.className = 'delete-item secondary-content'
   //Add icon
-  link.innerHTML = '<i class = " fa fa-remove"></i>'
+  link.innerHTML = '<i class="fa fa-remove"></i>'
   //Append the link to the li
   li.appendChild(link);
 
@@ -53,4 +59,53 @@ function addTask(e) {
   // form.addEventListener('submit', function () {
   //   taskInput.value = "";
   // });
+}
+
+//Add remove task function
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove(); //removes the li => parent of parent of icon tag
+    }
+  }
+  // console.log(e.target);
+}
+
+//Add clear tasks function
+function clearTasks(e) {
+  // Option 1 - delete all innerhtml from ul
+  //inner html vs remove child- google article, delete all inner html is slower than looping and removing child one by one
+
+  // taskList.innerHTML = '';
+
+  //Option 2 - while there asre still li children in ul, loop through the children and remove them
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  //Option 3 - my option,  retrieve all lis from ul as an html collection and convert them to an array so we can target each li with forEach and remove.
+  //Slower than all of the above.
+
+  // let liArr = [...taskList.children]
+  // console.log(liArr);
+  // console.log(taskList.children);
+  // liArr.forEach(li => li.remove())
+
+  // console.log(e.target);
+}
+
+//Add filter tasks function
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  //query selector returns a node list so we can use forEach
+  document.querySelectorAll('.collection-item').forEach(task => {
+    const item = task.firstChild.textContent;
+    console.log(task);
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  })
+  console.log(text);
 }
